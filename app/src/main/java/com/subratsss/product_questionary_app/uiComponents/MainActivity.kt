ba162055b1f92cb8991a083158fcff12c3199db6
subratsss <subratshankar.sahu@gmail.com>
2020-11-07 08:46:10 +0530

    setup recycler adapter with viewmodel and implement data binding in activity

--- app/src/main/java/com/subratsss/product_questionary_app/uiComponents/MainActivity.kt
+++ app/src/main/java/com/subratsss/product_questionary_app/uiComponents/MainActivity.kt
@@ -2,11 +2,48 @@ package com.subratsss.product_questionary_app.uiComponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.subratsss.product_questionary_app.R
+import com.subratsss.product_questionary_app.databinding.ActivityMainBinding
+import com.subratsss.product_questionary_app.viewModel.QuestionInfoViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var viewModel: QuestionInfoViewModel
+    private lateinit var recyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ViewModel
+        viewModel = ViewModelProvider(this).get(QuestionInfoViewModel::class.java)
+        viewModel.init()
+        viewModel.getProductRepository().observe(this,
+            Observer {
+                viewModel.setQuestionListInAdapter(it.questions)
+            })
+
+        //Data Binding
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        activityMainBinding.viewModel = viewModel
+
+        setupRecyclerView()
+    }
+
+    /**
+     * Setup RecyclerView
+     */
+    private fun setupRecyclerView() {
+        recyclerView = activityMainBinding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.isNestedScrollingEnabled = true
     }
 }
